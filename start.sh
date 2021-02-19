7571eb2d5bf5820588b6e2f31273540444da7530
Akhilesh Vyas <achilles4in@gmail.com>
2021-02-19 22:23:23 +0100

    updated start

--- start.sh
+++ start.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
-trap "exit" INT TERM ERR
+#trap "exit" INT TERM ERR
 
-trap "kill 0" EXIT
+#trap "kill 0" EXIT
 
-HOME='/nfs/home/vyasa/projects/proj_os/repos/bern'
+HOMEE='/nfs/home/vyasa/projects/proj_os/repos/bern'
 
 echo '##################################Directroy#############################################################'
 
 pwd
 
-cd $HOME/GNormPlusJava
-java -Xmx16G -Xms16G -jar GNormPlusServer.jar 18895 >> $HOME/logs/nohup_gnormplus.out 2>&1 &
+cd $HOMEE/GNormPlusJava
+java -Xmx16G -Xms16G -jar GNormPlusServer.jar 18895 >> $HOMEE/logs/nohup_gnormplus.out 2>&1 &
 
-cd $HOME/tmVarJava
-java -Xmx8G -Xms8G -jar tmVar2Server.jar 18896 >> $HOME/logs/nohup_tmvar.out 2>&1 &
+cd $HOMEE/tmVarJava
+java -Xmx8G -Xms8G -jar tmVar2Server.jar 18896 >> $HOMEE/logs/nohup_tmvar.out 2>&1 &
 
-cd $HOME
+cd $HOMEE
 sh load_dicts.sh &
 
 echo '######################### CUDA_VISIBLE_DEVICES ##############################'
@@ -24,6 +24,6 @@ echo $CUDA_VISIBLE_DEVICES
 export CUDA_VISIBLE_DEVICES=0
 
 
-python3 -u server.py --port 8888 --gnormplus_home $HOME/GNormPlusJava --gnormplus_port 18895 --tmvar2_home $HOME/tmVarJava --tmvar2_port 18896 >> $HOME/logs/nohup_BERN.out 2>&1 &
+python3 -u server.py --port 8888 --gnormplus_home $HOMEE/GNormPlusJava --gnormplus_port 18895 --tmvar2_home $HOMEE/tmVarJava --tmvar2_port 18896 >> $HOMEE/logs/nohup_BERN.out 2>&1 &
 
-tail -F $HOME/logs/nohup_BERN.out &
+tail -F $HOMEE/logs/nohup_BERN.out &
